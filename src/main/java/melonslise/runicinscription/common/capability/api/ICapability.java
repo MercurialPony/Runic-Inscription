0138dd17e933a0d928639b10364c1020cf15b6bb
Melonslise <arturzartdinov@gmail.com>
2017-09-06 23:11:13 +0300

    Update and rename ICapabilityBase.java to ICapability.java

--- src/main/java/melonslise/runicinscription/common/capability/api/ICapability.java
+++ src/main/java/melonslise/runicinscription/common/capability/api/ICapability.java
@@ -3,14 +3,12 @@ package melonslise.runicinscription.common.capability.api;
 import net.minecraft.util.ResourceLocation;
 
 /**
- *  Extended by other capabilities for use in the mod's capability provider API.
+ * Extended by other capabilities for use in the mod's capability API.
  */
-public interface ICapabilityBase // TODO Replace with holder
+public interface ICapability // TODO Replace with holder
 {
 	/**
 	 * Retrieves the capability's unique ID in form of a ResourceLocation.
-	 * 
-	 * @return A ResourceLocation containing a unique ID and preferably the mod's ID.
 	 */
 	public ResourceLocation getID();
 
