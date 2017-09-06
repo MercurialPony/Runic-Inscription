eb09d197079a11cced42d45514b13029dd76a035
Melonslise <arturzartdinov@gmail.com>
2017-09-06 23:10:35 +0300

    Update CapabilityStorage.java

--- src/main/java/melonslise/runicinscription/common/capability/api/CapabilityStorage.java
+++ src/main/java/melonslise/runicinscription/common/capability/api/CapabilityStorage.java
@@ -6,16 +6,16 @@ import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.Capability.IStorage;
 
 public class CapabilityStorage<T extends ICapabilitySerializable> implements IStorage<T>
-{	
+{
 	@Override
 	public NBTBase writeNBT(Capability<T> capability, T instance, EnumFacing side)
 	{
-		return ((ICapabilitySerializable) instance).writeNBT(side);
+		return instance.writeNBT(side);
 	}
 
 	@Override
 	public void readNBT(Capability<T> capability, T instance, EnumFacing side, NBTBase nbt)
 	{
-		((ICapabilitySerializable) instance).readNBT(nbt);
+		instance.readNBT(nbt);
 	}
 }
